bde66eac05df30d3b6504121585e705785b92e3e
Henrik Kniberg <h@kniberg.com>
2017-02-15 04:59:51 +0000

    Made it work better with parallell processes writing to the display. Still a bit flaky though, at least when doing frequent screen updates.

--- src/display/display_driver.js
+++ src/display/display_driver.js
@@ -1,29 +1,30 @@
 const i2c = require('i2c-bus')
-const oled = require('oled-i2c-bus');
+const oledBus = require('oled-i2c-bus');
 
 const font = require('oled-font-5x7');
 
 class DisplayDriver {
   constructor(busNumber, address, width, height) {
-    const bus = i2c.openSync(busNumber ? busNumber : 1)
-
-    var opts = {
-      width: width ? width : 128,
-      height: height ? height : 64,
-      address: address ? address : 0x3C
-    };
-
-    this.width = opts.width
-    this.height = opts.height
-
-    this.oled = new oled(bus, opts);
+    this.busNumber = busNumber ? busNumber : 1
+    this.address = address ? address : 0x3C
+    this.width = width ? width : 128
+    this.height = height ? height : 64
   }
 
   text(message) {
-    this.oled.fillRect(0, 0, this.width, this.height, 0);
-    this.oled.setCursor(1,1)
-    this.oled.writeString(font, 1, message, 1, true)
-    this.oled.update()
+    const bus = i2c.openSync(this.busNumber)
+
+    var opts = {
+      width: this.width,
+      height: this.height,
+      address: this.address
+    };
+
+    const oled = new oledBus(bus, opts);
+    oled.fillRect(0, 0, this.width, this.height, 0);
+    oled.setCursor(1,1)
+    oled.writeString(font, 1, message, 1, true)
+    oled.update()
   }
 }
 
