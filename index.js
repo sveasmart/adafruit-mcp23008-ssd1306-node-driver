24478df3d59f06d0a7e30d7cd4bee25932add183
Henrik Kniberg <h@kniberg.com>
2017-02-16 13:14:06 +0100

    Better at detecting when i2c isn't installed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 exports.ButtonDriver = require("./src/button/button_driver")
 exports.FakeButtonDriver = require("./src/button/fake_button_driver")
 
@@ -9,10 +11,17 @@ exports.demo = require("./src/demo/demo")
 exports.hasDriver = function() {
   try {
     require('i2c-bus')
-    return true
   } catch (err) {
+    console.log("I'll assume i2c isn't available, because I got this error when doing require('i2c-bus'): " + err)
     return false
   }
+
+  if (!fs.existsSync('/dev/i2c-1')) {
+    console.log("/dev/i2c-1 doesn't exist, so I'll assume i2c isn't available")
+    return false
+  }
+
+  return true
 }
 
 
